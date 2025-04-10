0b926e06088bbdbe622105acce0f1e137d149ed0
nithish <nithishtulalu121@gmail.com>
2025-04-10 22:55:04 +0530

    code5

---
+++ CodeBase/CodeBase/Code4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CodeBase
+{
+    public class Code4
+    {
+
+        //Palindrome number 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a  number");
+            int num = int.Parse(Console.ReadLine());
+            int original = num;
+            int rev = 0;
+            while (num > 0)
+            {
+                rev = rev * 10 + num % 10;
+                num = num / 10;
+
+            }
+            Console.WriteLine(rev);
+            if (rev == original)
+            {
+                Console.WriteLine("given  number is   palndrom  number");
+            }
+            else
+            {
+
+                Console.WriteLine("given  number is  not  palndrom  number");
+            }
+
+        }
+    }
+}
---
+++ CodeBase/CodeBase/Code5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CodeBase
+{
+    public class Code5
+    {
+
+        //Palindrome string
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter  a  string");
+            string str = Console.ReadLine();
+            string org = str;
+            string rev = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                rev = rev + str[i];
+
+            }
+            if (rev == org)
+            {
+                Console.WriteLine("Palindrome string");
+            }
+            else
+            {
+                Console.WriteLine("not   Palindrome string");
+            }
+
+        }
+    }
+}
