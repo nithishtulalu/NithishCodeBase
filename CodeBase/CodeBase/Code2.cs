563d62a5538844effe3859200f3384052b7038bd
nithish <nithishtulalu121@gmail.com>
2025-04-10 22:37:39 +0530

    code sub

---
+++ CodeBase/CodeBase/Code2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CodeBase
+{
+    //revers  a number  
+    public class Code2
+    {
+     public static void Main(string[] args)
+        {
+            Console.WriteLine("enter a  number");
+            int num= int.Parse(Console.ReadLine());
+            int rev = 0;
+
+            while (num > 0)
+            {
+                rev = rev * 10 + num % 10;
+                num = num / 10;
+
+            }
+            Console.WriteLine("the   Reveres number:"+rev);
+        }
+
+    }
+}
---
+++ CodeBase/CodeBase/Code3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CodeBase
+{
+    //revers a String
+    public class Code3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter  a  string");
+            string str = Console.ReadLine();
+            String rev = "";
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                rev = rev + str[i];
+
+            }
+            Console.WriteLine(rev);
+        }
+    }
+    }
