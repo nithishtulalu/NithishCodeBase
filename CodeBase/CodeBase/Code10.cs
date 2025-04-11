8b2ff1224cbe4843cebf81896f9b539f2655fc72
nithish <nithishtulalu121@gmail.com>
2025-04-11 18:49:35 +0530

    nA

---
+++ CodeBase/CodeBase/Code10.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CodeBase
+{
+    //largest  among   three numbers
+    public class Code10
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter  Num1");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter  Num2");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter  Num3");
+            int num3 = int.Parse(Console.ReadLine());
+
+
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine(num1 + "is gratter");
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine(num2 + "is gratter");
+
+            }
+
+            else {
+                Console.WriteLine(num3+ "is gratter");
+            }
+        }
+
+    }
+}
---
+++ CodeBase/CodeBase/Code9.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CodeBase
+{
+    // Count number of odd and even digits in a number
+    public class Code9
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int num = int.Parse(Console.ReadLine());
+            int evencount = 0;
+            int oddcount = 0;
+
+            
+
+            while (num > 0)
+            {
+                int digt = num % 10;
+                if (digt % 2 == 0)
+                {
+                    Console.WriteLine( "Even:"+digt);
+                    evencount++;
+                }
+                else
+                {
+                    Console.WriteLine("odd :"+digt);
+                    oddcount++;
+                }
+                num = num / 10;
+
+
+            }
+
+            Console.WriteLine("Even digit count: " + evencount);
+            Console.WriteLine("Odd digit count: " + oddcount);
+        }
+    }
+}
