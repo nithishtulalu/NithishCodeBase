5518a17e39d5f2d968dcf5c0d5e70848a77edfbd
nithish <nithishtulalu121@gmail.com>
2025-04-12 15:27:23 +0530

    Prime

---
+++ CodeBase/CodeBase/Code26.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CodeBase
+{
+    public class Code26
+    // revers each  word in  a string  
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+
+            string[] words = str.Split(new char[] { ' ' });
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                char[] chars = words[i].ToCharArray();
+                Array.Reverse(chars);
+                words[i] = new string(chars);
+
+            }
+            string result= string.Join(" ", words);
+            Console.WriteLine(result);  
+
+
+        }
+    }
+}
---
+++ CodeBase/CodeBase/Code27.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CodeBase
+{
+    // Prime numbers within a range
+    public class Code27
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a start number:");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter an end number:");
+            int end = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Prime numbers between {start} and {end} are:");
+
+            for (int i = start; i <= end; i++)
+            {
+                bool isPrime = true;
+
+                if (i <= 1)
+                {
+                    isPrime = false;
+                }
+                else
+                {
+                    for (int j = 2; j <= i / 2; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
