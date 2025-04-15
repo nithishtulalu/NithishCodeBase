64849cf866cb1eeefd43bb12a8c774f243aff1ff
nithish <nithishtulalu121@gmail.com>
2025-04-15 22:56:26 +0530

    LIM

---
+++ CodeBase/CodeBase/Code28.cs
@@ -0,0 +1,26 @@
+﻿using System;
+//find  largest  word in  string 
+namespace CodeBase
+{
+    public class Code28
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a  string");
+            string str = Console.ReadLine();
+
+            string[] words = str.Split(' ');
+            string maxword = " ";
+            int maxlen = 0;
+            foreach (string word in words)
+            {
+                if(word.Length > maxlen)
+                {
+                    maxlen = word.Length;
+                    maxword = word;
+                }
+            }
+            Console.WriteLine("the  MAX Word::" + maxword);
+        }
+    }
+}
---
+++ CodeBase/CodeBase/Code29.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CodeBase
+{
+    //min word in a string
+    public class Code29
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+
+            string[] words = str.Split(' ');
+            string minword = " ";
+            int len = int.MaxValue;
+
+            foreach (string word in words)
+            {
+                if (word.Length < len)
+                {
+                    len = word.Length;
+                    minword = word;
+                }
+            }
+
+            Console.WriteLine("Minmum word is::"+minword);
+        }
+    }
+}
