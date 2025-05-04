72a19bddf5f08ca8a1889824fa68985842c69b72
nithish <nithishtulalu121@gmail.com>
2025-05-04 10:16:55 +0530

    code37

---
+++ CodeBase/CodeBase/Code36.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System;
+
+namespace CodeBase
+{
+    //Find the second highest element in an array
+    public class Code36
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int firstmax = arr[0];
+            int secondmax = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > firstmax)
+                {
+                    secondmax = firstmax;
+                    firstmax = arr[i];
+                }
+                else if (arr[i] < secondmax && secondmax != firstmax) { 
+                    secondmax=arr[i];
+                }
+            }
+            Console.WriteLine("Second max is :"+secondmax);
+
+
+
+        }
+    }
+}
---
+++ CodeBase/CodeBase/Code37.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CodeBase
+{
+    public class Code37
+    {
+        //Find the second smallest element in an array
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int firstmin = int.MaxValue;
+            int secondmin = int.MaxValue;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < firstmin) { firstmin = arr[i]; }
+                else if (arr[i] < secondmin && secondmin != firstmin)
+                {
+                    secondmin = arr[i];
+                }
+
+            }
+            Console.WriteLine("Second Minmum Number:" + secondmin);
+        }
+    }
+}
