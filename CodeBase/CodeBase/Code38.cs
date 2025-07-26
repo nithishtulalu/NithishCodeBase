8a00d4a4bcfa1187c45da71bf71ecbe2678e8899
nithish <nithishtulalu121@gmail.com>
2025-07-26 15:31:34 +0530

    Code 38

---
+++ CodeBase/CodeBase/Code38.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeBase
+{
+    //Count  Vowels in  a string ==AEIOU
+    public class Code38
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter A String");
+            string str= Console.ReadLine().ToUpper();
+            int count = 0;
+            char[] vowels = { 'A', 'E', 'I', 'O', 'U' };
+
+            foreach (char  c in str)
+            {
+                if (vowels.Contains(c))
+                {
+                    count++;
+                }
+            }
+
+
+            Console.WriteLine("Vowels  count "+count);
+        }
+    }
+}
